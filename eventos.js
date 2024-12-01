c622244a8acb5ffd0eccf5263694cb747481c2d6
Caro <saavedracaaro@gmail.com>
2024-12-01 18:40:01 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,4 +1,3 @@
-
 // Función para saludar con el botón
 function saludar() {
     alert('¡Hola!');
@@ -7,4 +6,10 @@ function saludar() {
 // Manejador de eventos para el clic en el div
 document.querySelector('.contenedor-boton').addEventListener('click', function() {
     alert('Hola! Soy el div');
+});
+
+// Manejador de eventos para el clic en el botón, evitando la propagación
+document.querySelector('button').addEventListener('click', function(event) {
+    alert('¡Hola!');
+    event.stopPropagation();  // Previene que el clic en el botón dispare el evento del div
 });
